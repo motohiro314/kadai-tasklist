e32cad15aaee77d1b5d5d86bb4d665206ad9ff63
motohiro314 <motom2707@gmail.com>
2020-12-14 17:01:16 +0000

    CRUD messages

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 // デフォルトのコメント部分は省略
 
+
+/*Route::get('/', function () {
+    return view('welcome');
+});*/
+
 Route::get('/', 'TasksController@index');
 
 Route::resource('tasks', 'TasksController');
